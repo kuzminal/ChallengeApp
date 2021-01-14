6c64e4985507ec97318b2ca4f3fe864e5b457b6c
kuzminal <kuzmin.al35@gmail.com>
2021-01-14 10:37:52 +0300

    Created domain and simple service

---
+++ src/main/java/com/kuzmin/multiplication/challenge/ChallengeGeneratorService.java
@@ -0,0 +1,8 @@
+package com.kuzmin.multiplication.challenge;
+
+public interface ChallengeGeneratorService {
+    /**
+     * @return a randomly-generated challenge with factors between 11 and 99
+     */
+    Challenge randomChallenge();
+}
---
+++ src/main/java/com/kuzmin/multiplication/user/User.java
@@ -0,0 +1,15 @@
+package com.kuzmin.multiplication.user;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public class User {
+    private Long id;
+    private String alias;
+}
